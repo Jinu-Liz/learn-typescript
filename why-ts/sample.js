95bc675a6cc94ec303a28023789fa6eb9e3ba7a5
Hyungseock-Yun <bagzein@gmail.com>
2023-12-21 02:38:23 +0900

    - JS를 TS처럼 코딩하는 법

--- why-ts/sample.js
+++ why-ts/sample.js
@@ -1,5 +1,15 @@
+// @ts-check    // typescript처럼 타입 체크
+
+/**
+ * 
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ * @returns 
+ */
 function sum(a, b) {
     return a + b;
 }
 
+
+sum(10, '20');
 // 30 !== sum(10, '20'); // 1020
