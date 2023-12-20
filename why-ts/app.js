4b773c091aac5bc77251e70787c3039198db4b1e
Hyungseock-Yun <bagzein@gmail.com>
2023-12-21 01:36:36 +0900

    - 에러 방지 - 타입 정의하여 자동완성

--- why-ts/app.js
+++ why-ts/app.js
@@ -9,9 +9,32 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
+fetchUser().then(function (response) {
+  response.
+})
+
 function startApp() {
-  axios
-    .get(url)
+  fetchUser()
     .then(function (response) {
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
@@ -20,7 +43,7 @@ function startApp() {
       email.innerText = user.email;
       // address.innerText = user.address;  // address가 Object이기 때문에 [Object object]로 나옴
       // address.innerText = user.addres;  // 오탈자가 난 경우, undefined로 나옴
-      address.innerText = user.address.street;
+      address.innerText = user.address.city;
     })
     .catch(function (error) {
       console.log(error);
