bd270d54d874b5ebb8a5b60f5f5a1df133552833
hsyoon <bagzein@gmail.com>
2023-12-24 13:52:06 +0900

    - 인터페이스 > 함수의 인자/구조를 정의 > 인덱싱 방식을 정의

---
+++ example/getting-started/class-note/3_interface.ts
@@ -0,0 +1,39 @@
+interface User {
+    age: number;
+    name: string;
+}
+
+// 변수에 인터페이스 활용
+var seho: User = {
+    age: 33,
+    name: '세호'
+}
+
+// 함수에 인터페이스 활용
+function getUser(user: User) {
+    console.log(user);
+}
+
+const capt = {
+    age: 50,
+    name: '캡틴'
+}
+getUser(capt);
+
+// 함수의 스펙(구조)에 인터페이스를 활용
+interface SumFunction {
+    (a: number, b: number): number;
+}
+
+var summary: SumFunction;
+summary = function(a: number, b: number): number {
+    return a + b;
+}
+
+// 인덱싱
+interface StringArray {
+    [index: number]: string;
+}
+
+var arr1: StringArray = ['a', 'b', 'c'];
+arr1[0] = 10; // 'a'
