c75138380ff5a4cb87b8916a668ea8d360ee0fa3
hsyoon <bagzein@gmail.com>
2023-12-24 14:40:00 +0900

    - 연산자를 이용한 타입 정의 > 유니온 타입의 특징

--- example/getting-started/class-note/5_operator.ts
+++ example/getting-started/class-note/5_operator.ts
@@ -22,3 +22,20 @@ function logMessage(value: string | number) {
 
 logMessage('hello');
 logMessage(100);
+
+interface Developer2 {
+    name: string;
+    skill: string;
+}
+
+interface Person2 {
+    name: string;
+    age: number;
+}
+
+// 복수의 인터페이스를 사용할 경우, 공통 인자들만 자동완성이 된다. (타입 세이프)
+function askSomeone(someone: Developer2 | Person2) {
+    someone.name;
+    someone.age;    // 에러뜸
+    someone.skill;  // 에러뜸
+}
