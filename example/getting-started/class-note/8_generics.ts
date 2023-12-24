03c51b0519cb85b7843aee1b2503924c23faf8ab
hsyoon <bagzein@gmail.com>
2023-12-24 16:25:30 +0900

    - 제네릭 > 제네릭의 장점 및 타입 추론에서의 이점

--- example/getting-started/class-note/8_generics.ts
+++ example/getting-started/class-note/8_generics.ts
@@ -32,13 +32,32 @@
 // }
 
 // 유니온의 경우, 공통 API만 자동완성으로 뜬다.
-function logText(text: string | number) {
+// function logText(text: string | number) {
+//     console.log(text);
+//     // text.split('').reverse().join('');
+    
+//     return text;
+// }
+// const a = logText('a');
+// a.split('');    // number가 들어오는 경우, split을 사용할 수 없으므로 에러가 뜬다.
+// logText(10);
+// logText(true);
+
+
+/**
+ * 함수명을 변경하여 별도로 선언하지 않아도, 타입을 유동적으로 지정하여 해당 타입의 API를 사용할 수 있다. 
+ * 제네릭은 타입에 무엇이 들어갈 지 호출한 시점에 정하여 타입을 추론해서 최종 반환값까지 붙일 수 있다.
+ */
+function logText<T>(text: T): T {
     console.log(text);
-    // text.split('').reverse().join('');
     
     return text;
 }
-const a = logText('a');
-a.split('');    // number가 들어오는 경우, split을 사용할 수 없으므로 에러가 뜬다.
-logText(10);
-// logText(true);
+
+const abc = logText<string>('abc');
+abc.split('');
+
+const login = logText<boolean>(true);
+login.valueOf();
+
+logText<number>(10);
