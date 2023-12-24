52a440e9bcead50f7b2678e348c482acbf668c27
hsyoon <bagzein@gmail.com>
2023-12-24 16:15:35 +0900

    - 제네릭 > 유니온 타입을 사용했을 때의 문제점

--- example/getting-started/class-note/8_generics.ts
+++ example/getting-started/class-note/8_generics.ts
@@ -18,18 +18,27 @@
 
 
 // logText, logNumber은 내용은 같은 로직이지만 타입 때문에 별도로 함수를 선언. 유지보수에 좋지 않음.
-function logText(text: string) {
+// function logText(text: string) {
+//     console.log(text);
+//     // text.split('').reverse().join('');
+    
+//     return text;
+// }
+
+// function logNumber(num: number) {
+//     console.log(num);
+    
+//     return num;
+// }
+
+// 유니온의 경우, 공통 API만 자동완성으로 뜬다.
+function logText(text: string | number) {
     console.log(text);
     // text.split('').reverse().join('');
     
     return text;
 }
-
-function logNumber(num: number) {
-    console.log(num);
-    
-    return num;
-}
-logText('a');
-logNumber(10);
-logText(true);
+const a = logText('a');
+a.split('');    // number가 들어오는 경우, split을 사용할 수 없으므로 에러가 뜬다.
+logText(10);
+// logText(true);
