4c1d7768188acf91a25bd27444e4f4ab6a6ba58d
hsyoon <bagzein@gmail.com>
2023-12-31 14:53:38 +0900

    - 타입 모듈화 > 모듈화 실습

---
+++ quiz/2_address-book/src/types.ts
@@ -0,0 +1,20 @@
+interface PhoneNumberDictionary {
+    [phone: string]: {
+      num: number;
+    };
+  }
+  
+interface Contact {
+    name: string;
+    address: string;
+    phones: PhoneNumberDictionary;
+  }
+
+  
+enum PhoneType {
+    HOME = 'home',
+    OFFICE = 'office',
+    STUDIO = 'studio',
+  }
+
+export { Contact, PhoneType };
