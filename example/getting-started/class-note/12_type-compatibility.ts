3463bb325931905fd4d9fc8c82a6ce11252efef1
hsyoon <bagzein@gmail.com>
2023-12-31 13:41:46 +0900

    - 타입 호환 > 타입 호환 예제(함수, 제네릭)

--- example/getting-started/class-note/12_type-compatibility.ts
+++ example/getting-started/class-note/12_type-compatibility.ts
@@ -34,3 +34,35 @@ var person2: Person2;
 
 developer2 = person2;   // interface는 다르지만 속성(구조)이 같으므로 동일하다고 판단. 구조적 타이핑(structural typing)
 developer2 = new Person4();     // 구조가 같으면 class 할당도 가능.
+
+// 함수
+var add11 = function(a: number) {
+    // ...
+}
+
+var sum11 = function(a: number, b: number) {
+    // ...
+}
+
+sum11 = add11;  // sum의 인자 수가 더 많아 범위가 더 넓으므로 호환 가능하다고 판단.
+add11 = sum11;  // add의 인자 수가 더 적으므로 에러.
+
+
+// 제네릭
+interface Empty<T> {
+    // ...
+}
+var empty1: Empty<string>;
+var empty2: Empty<number>;
+empty1 = empty2;
+empty2 = empty2;
+
+interface NotEmpty<T> {
+    data: T;
+}
+
+// 속성의 타입이 선언된 타입에 따라 달라지기 때문에 다른 타입이 선언되면 에러가 발생한다.
+var notEmpty1: NotEmpty<string>;
+var notEmpty2: NotEmpty<number>;
+notEmpty1 = notEmpty2;
+notEmpty2 = notEmpty1;
