e121ef84978ae98b3443690d624fc0275ce80aa8
hsyoon <bagzein@gmail.com>
2023-12-31 13:12:43 +0900

    - 타입 가드 > 타입 단언만을 사용했을 때의 문제점

---
+++ example/getting-started/class-note/11_type-guard.ts
@@ -0,0 +1,27 @@
+interface Developer {
+    name: string;
+    skill: string;
+}
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+function introduce(): Developer | Person {
+    return { name: 'Tony', age: 33, skill: 'Iron Making'}
+}
+
+var tony = introduce();
+console.log(tony.skill);    // 공통 속성만 접근가능하기 때문에 skill은 에러남
+
+// 이렇게 타입별/속성별 존재유무를 판단하는 것도 좋지만 가독성 및 반복되는 코드로 인해 효율이 떨어짐.
+if ((tony as Developer).skill) {
+    var skill = (tony as Developer).skill;
+    console.log(skill);
+    
+} else if ((tony as Person).age) {
+    var age = (tony as Person).age;
+    console.log(age);
+    
+}
